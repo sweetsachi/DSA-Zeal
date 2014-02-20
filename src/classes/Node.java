88ddf3ef62d19598a97da9c548ded405075ce469
sweetsachi <replytosachini@gmail.com>
2014-02-20 20:37:29 +0530

    first commit

---
+++ src/classes/Node.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author HP
+ */
+public class Node {
+    
+    private String title;
+    private String authorName;
+    private String authorSurname;
+    private int isbn;
+    
+    private Node left;
+    private Node right;
+    
+    public Node (String title, String authorName,String authorSurname,int isbn )
+    {
+        this.title = title;
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+        this.isbn = isbn;
+        
+        this.left = null;
+        this.right = null;
+    }
+    
+    public Node()
+    {}
+    
+    public Node getLeft()
+    {
+        return this.left;
+    }
+    
+    public Node getRight()
+    {
+        return this.right;
+    }
+    
+    public String getTitle()
+    {
+        return this.title;
+    }
+    
+    public int getIsbn()
+    {
+        return this.isbn;
+    }
+            
+   public void insert( Node newNode)
+   {
+       
+   }
+            
+}
